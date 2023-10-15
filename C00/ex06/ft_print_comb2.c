8a3ea124c2c48afd44df962e354d29a2506026d5
killuazol22 <oussama.business23@gmail.com>
2023-10-15 13:45:32 +0100

    C00

---
+++ C00/ex06/ft_print_comb2.c
@@ -0,0 +1,39 @@
+#include <unistd.h>
+void ft_putchar(char c)
+{
+    write (1,&c,1);
+}
+
+void ft_print_comb2(void)
+{
+  int i;
+  int j;
+  
+  i = 0;
+  while (i <= 98)
+  {
+    j = i + 1;
+    while (j<= 99)
+    {
+        ft_putchar((i / 10) + 48);
+        ft_putchar((i % 10) + 48);
+        ft_putchar(' ');
+        ft_putchar((j / 10) + 48);
+        ft_putchar((j % 10) + 48);
+      if (i != 98 || j != 99)
+      {
+        ft_putchar(',');
+        ft_putchar(' ');
+      }
+      j++;
+    }
+    i++;
+  }
+}
+
+
+int main() 
+{
+  ft_print_comb2();
+  return 0;
+}
